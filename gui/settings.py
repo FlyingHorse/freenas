d7d17aa408e6b1fb980707cff8af76ea072699f8
刘阳阳 <liuyangyang@lyys-mbp.local>
2016-12-07 13:51:39 +0800

    timezone and language

--- gui/settings.py
+++ gui/settings.py
@@ -89,11 +89,13 @@ if os.path.exists(DATABASE_PATH):
 # although not all choices may be available on all operating systems.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = None
+#TIME_ZONE = None
+TIME_ZONE = 'Asia/Shanghai'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh_CN'
 
 SITE_ID = 1
 
